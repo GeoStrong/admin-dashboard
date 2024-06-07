66b17e08a1bbfd276d7399a60ad15e14d0c96d2e
GeoStrong <dzobavageorgij@gmail.com>
2024-06-07 15:31:03 +0400

    Update list component styles and remove marginTop prop

--- components/general/list/list.js
+++ components/general/list/list.js
@@ -1,9 +1,9 @@
 import ActiveLink from "./active-link";
 
-const List = ({ links, borderBottom, marginTop }) => {
+const List = ({ links, borderBottom }) => {
   return (
     <ul
-      className={`mt-${marginTop} flex flex-col ${borderBottom && "border-b-[1px] border-gray-200"} px-6`}
+      className={`mt-5 flex flex-col ${borderBottom && "border-b-[1px] border-gray-200"} px-6`}
     >
       {links.map((link) => (
         <ActiveLink key={link.name} link={link} />
--- components/general/navbar.js
+++ components/general/navbar.js
@@ -6,15 +6,15 @@ import { accountLinks, mainLinks, pagesLinks } from "@/lib/dummy-database";
 const Navbar = () => {
   return (
     <nav className="nav-position flex h-screen w-full flex-col bg-white pt-10 dark:bg-dark-50">
-      <Link href="/" className="flex justify-center">
+      <Link href="/" className="mb-3 flex justify-center">
         <LogoImage />
       </Link>
-      <List links={mainLinks} borderBottom={true} marginTop="14" />
-      <p className="mt-4 px-10 text-xl font-bold text-grey-50 dark:text-white">
+      <List links={mainLinks} borderBottom={true} />
+      <p className="mt-5 px-10 text-xl font-bold text-grey-50 dark:text-white">
         Pages
       </p>
-      <List links={pagesLinks} borderBottom={true} marginTop="6" />
-      <List links={accountLinks} borderBottom={false} marginTop="6" />
+      <List links={pagesLinks} borderBottom={true} />
+      <List links={accountLinks} borderBottom={false} />
     </nav>
   );
 };
