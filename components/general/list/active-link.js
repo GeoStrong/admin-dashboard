69a45ec9de2efef653cfc4fc3bc5135885d21824
GeoStrong <dzobavageorgij@gmail.com>
2024-06-07 15:31:56 +0400

    Update list component styles and add fade animation to active link

--- components/general/list/active-link.js
+++ components/general/list/active-link.js
@@ -6,13 +6,13 @@ import { usePathname } from "next/navigation";
 
 const ActiveLink = ({ link }) => {
   const pathname = usePathname();
-  const activeLink = pathname === link.href;
+  const activeLink = pathname.includes(link.href);
 
   return (
     <>
       <motion.li
         key={link.name}
-        className={`mb-4 rounded-lg text-dark-100 dark:text-white ${activeLink ? "active-link text-white" : ""}`}
+        className={`mb-4 rounded-lg text-dark-100 dark:text-white ${activeLink ? "active-link animate-fade-left text-white" : ""}`}
       >
         <Link href={link.href} className={`flex items-center gap-4 p-4 `}>
           {link.icon} {link.name}
