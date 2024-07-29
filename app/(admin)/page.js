10a1be3d58de330ad68351fbb769cb0fb3029091
GeoStrong <dzobavageorgij@gmail.com>
2024-07-29 21:34:33 +0400

    Update page.js to improve responsiveness and readability

--- app/(admin)/page.js
+++ app/(admin)/page.js
@@ -1,8 +1,8 @@
 const Overview = () => {
   return (
     <>
-      <h2 className="text-center text-4xl font-bold">Hello</h2>
-      <p className="mt-6 text-center text-3xl">
+      <h2 className="text-center font-bold md:text-4xl">Hello</h2>
+      <p className="mt-6 text-center md:text-3xl">
         This website is currently under development. You can access the
         dashboard and some features now, with more features coming soon.
       </p>
