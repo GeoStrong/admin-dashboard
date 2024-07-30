2bbffccf6b94fc118962a1b869d10986ba826a3a
GeoStrong <dzobavageorgij@gmail.com>
2024-07-30 19:29:26 +0400

    Add vibration feedback to favorite button click

--- components/products/favorite-button.js
+++ components/products/favorite-button.js
@@ -3,13 +3,15 @@
 import { useState } from "react";
 import { AiFillHeart } from "react-icons/ai";
 import { AiOutlineHeart } from "react-icons/ai";
+import { Haptics, ImpactStyle } from "@capacitor/haptics";
 
 const FavoriteButton = () => {
   const [isFavorite, setIsFavorite] = useState(false);
 
-  const handleFavorite = () => {
+  const handleFavorite = async () => {
     setIsFavorite(!isFavorite);
     navigator.vibrate([200, 100, 200]);
+    await Haptics.vibrate();
   };
 
   return (
@@ -17,7 +19,7 @@ const FavoriteButton = () => {
       className="cursor-pointer rounded-full bg-gray-100 p-4 dark:bg-dark-150"
       onClick={handleFavorite}
     >
-      {isFavorite ? <AiFillHeart fill="black" /> : <AiOutlineHeart />}{" "}
+      {isFavorite ? <AiFillHeart fill="red" /> : <AiOutlineHeart />}{" "}
     </div>
   );
 };
