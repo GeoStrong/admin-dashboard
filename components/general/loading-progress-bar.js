576533412d34b0b0022d273a2421cbb71521911d
GeoStrong <dzobavageorgij@gmail.com>
2024-08-15 23:53:08 +0400

    Update LoadingProgressBar component to remove unnecessary options and improve code readability

--- components/general/loading-progress-bar.js
+++ components/general/loading-progress-bar.js
@@ -6,12 +6,7 @@ const LoadingProgressBar = ({ children }) => {
   return (
     <>
       {children}
-      <ProgressBar
-        height="4px"
-        color="#4880ff"
-        options={{ showSpinner: false }}
-        shallowRouting
-      />
+      <ProgressBar height="4px" color="#4880ff" shallowRouting />
     </>
   );
 };
