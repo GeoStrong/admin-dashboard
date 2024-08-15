04eaee884f55b0ef6a50bc41c266297f07fc3a15
GeoStrong <dzobavageorgij@gmail.com>
2024-08-15 23:22:51 +0400

    Update Inbox page layout and components

--- app/(admin)/main/inbox/page.js
+++ app/(admin)/main/inbox/page.js
@@ -1,15 +1,14 @@
 import InboxComposeButton from "@/components/inbox/inbox-compose-button";
-import InboxMailTools from "@/components/inbox/inbox-mail-tools";
+import InboxDesktopContent from "@/components/inbox/inbox-desktop-content";
 import InboxMenuLinks from "@/components/inbox/inbox-menu-links";
-import InboxMessages from "@/components/inbox/inbox-messages";
+import InboxMobileContent from "@/components/inbox/inbox-mobile-content";
 import InboxMobileMenu from "@/components/inbox/inbox-mobile-menu";
-import InboxSearchInput from "@/components/inbox/input-search-input";
 
 const Inbox = () => {
   return (
     <>
       <h2 className="text-2xl font-bold">Inbox</h2>
-      <div className="flex w-full justify-between gap-3">
+      <div className="flex w-full flex-col justify-between gap-3 md:flex-row">
         <div className="mt-2 w-full rounded-xl bg-gray-200 p-3 dark:bg-dark-50 md:w-1/3 md:p-6">
           <InboxMobileMenu />
           <div className="hidden md:block">
@@ -18,17 +17,8 @@ const Inbox = () => {
             <InboxMenuLinks />
           </div>
         </div>
-        <div className="mt-2 hidden rounded-xl bg-gray-200  dark:bg-dark-50 md:block md:w-2/3 ">
-          <div className="block items-center justify-between p-3 md:p-6 lg:flex">
-            <div className="lg:w-1/2">
-              <InboxSearchInput />
-            </div>
-            <div className="hidden lg:block">
-              <InboxMailTools />
-            </div>
-          </div>
-          <InboxMessages />
-        </div>
+        <InboxMobileContent />
+        <InboxDesktopContent />
       </div>
     </>
   );
