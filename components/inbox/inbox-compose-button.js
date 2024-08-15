6bce95aa4569f8129eeaa8d0ffc704004b7d294f
GeoStrong <dzobavageorgij@gmail.com>
2024-08-15 23:21:14 +0400

    Update CSS class names for active link background and compose button shadow

--- components/inbox/inbox-compose-button.js
+++ components/inbox/inbox-compose-button.js
@@ -3,7 +3,7 @@ import { Button } from "../general/UI/button";
 
 const InboxComposeButton = () => {
   return (
-    <Button className="flex items-center gap-1 rounded-lg bg-links-background/90 text-white dark:hover:bg-links-background/30 md:w-full">
+    <Button className="compose-shadow flex items-center gap-1 rounded-lg bg-links-background/90 text-white dark:hover:bg-links-background/30 md:w-full md:shadow-none">
       <AiOutlinePlus className="text-xs" /> Compose
     </Button>
   );
