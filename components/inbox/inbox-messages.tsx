f4baad17648f191324a58012d40a32864e538391
GeoStrong <dzobavageorgij@gmail.com>
2024-12-22 20:11:44 +0400

    Refactor InboxMessages component to enhance state management and improve message handling logic

--- components/inbox/inbox-messages.tsx
+++ components/inbox/inbox-messages.tsx
@@ -4,14 +4,24 @@ import MailStarredIcon from "@/public/svg/mail-icons/mail-starred-icon";
 
 import { Checkbox } from "../general/UI/checkbox";
 import { Button } from "../general/UI/button";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useLongPress } from "react-use";
 import useScreenSize from "@/lib/hooks/useScreenSize";
-import { CheckboxState, RandomMessages } from "@/lib/dummy-database";
+import {
+  defaultOptions,
+  InboxMessageState,
+  RandomMessages,
+  ReactDispatchState,
+} from "@/lib/dummy-database";
+import { useAppDispatch, useAppSelector } from "@/lib/store/redux-hooks";
+import { inboxMessagesAction } from "@/lib/store/inbox-messages-slice";
+import { retrieveDate, toggleMessageStatus } from "@/lib/functions/functions";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "../general/UI/avatar";
 
 interface InboxMessagesProps {
-  messageCheckStatus: CheckboxState | boolean;
-  setMessageCheckStatus: React.Dispatch<React.SetStateAction<CheckboxState>>;
+  messageCheckStatus: InboxMessageState | boolean;
+  setMessageCheckStatus: ReactDispatchState;
   dividedMessages: RandomMessages[];
   displayTools: boolean;
   setDisplayTools: (display: boolean) => void;
@@ -24,34 +34,31 @@ const InboxMessages: React.FC<InboxMessagesProps> = ({
   displayTools,
   setDisplayTools,
 }) => {
-  const [isStarred, setIsStarred] = useState(false);
-  const { isXsm, isSm } = useScreenSize();
+  const dispatch = useAppDispatch();
+  const [starredMessages, setStarredMessages] = useState<InboxMessageState[]>();
+  const [activeMessageData, setActiveMessageData] = useState<RandomMessages>();
+  const { inbox } = useAppSelector((state) => state.activeSlug);
+  const { isMobile } = useScreenSize();
 
-  const isMobile = isXsm || isSm;
-
-  const toggleMessageCheckStatus = (id: string) => {
-    setMessageCheckStatus((prevState) => ({
-      ...prevState,
-      [id]: !messageCheckStatus[id],
-    }));
-  };
-
-  const onLongPress = (event: MouseEvent) => {
-    const target = event.target as HTMLDivElement;
-    const checkedId = target.getAttribute("checked-id");
-
-    toggleMessageCheckStatus(checkedId);
-  };
+  const findStarredMessage = useCallback(
+    (id: string) => {
+      return starredMessages && starredMessages[id];
+    },
+    [starredMessages],
+  );
 
   useEffect(() => {
     dividedMessages &&
       dividedMessages.map((message: RandomMessages) => {
-        setMessageCheckStatus((prevState) => ({
-          ...prevState,
-          [message.id]: false,
-        }));
+        toggleMessageStatus(message.id, setMessageCheckStatus, false);
+
+        toggleMessageStatus(
+          message.id,
+          setStarredMessages,
+          inbox === "starred" ? true : false,
+        );
       });
-  }, [dividedMessages, setMessageCheckStatus]);
+  }, [dividedMessages, inbox, setMessageCheckStatus]);
 
   useEffect(() => {
     const checkedItems =
@@ -65,22 +72,37 @@ const InboxMessages: React.FC<InboxMessagesProps> = ({
     setDisplayTools(checkedItems);
   }, [messageCheckStatus, setDisplayTools]);
 
-  const defaultOptions = {
-    isPreventDefault: true,
-    delay: 300,
+  useEffect(() => {
+    if (inbox === "starred" || !activeMessageData) return;
+    if (findStarredMessage(activeMessageData.id)) {
+      dispatch(
+        inboxMessagesAction.addMessages({
+          name: "starredMessages",
+          data: activeMessageData,
+        }),
+      );
+    } else {
+      dispatch(
+        inboxMessagesAction.removeMessage({
+          name: "starredMessages",
+          id: activeMessageData.id,
+        }),
+      );
+    }
+  }, [activeMessageData, dispatch, findStarredMessage, inbox]);
+
+  const onLongPress = (event: MouseEvent) => {
+    const target = event.target as HTMLDivElement;
+    const checkedId = target.getAttribute("checked-id");
+
+    toggleMessageStatus(
+      checkedId,
+      setMessageCheckStatus,
+      !messageCheckStatus[checkedId],
+    );
   };
 
   const longPressEvent = useLongPress(onLongPress, defaultOptions);
-
-  const retrieveDate = (date: string) => {
-    const dateObj = new Date(date);
-
-    return dateObj.toLocaleDateString("en-US", {
-      month: "short",
-      day: "numeric",
-      ...(isMobile ? {} : { year: "numeric" }),
-    });
-  };
 
   return (
     <div className="mt-3 flex flex-col" suppressHydrationWarning>
@@ -88,7 +110,7 @@ const InboxMessages: React.FC<InboxMessagesProps> = ({
         dividedMessages.map((message: RandomMessages) => (
           <div key={message.id} id={message.id}>
             <div
-              className={`relative flex items-center justify-between gap-3 rounded-lg rounded-b-none border border-x-0 border-t-0 border-gray-300 p-4 transition-all last:border-0 ${messageCheckStatus && messageCheckStatus[message.id] && "rounded-b-lg bg-gray-300 dark:bg-dark-150"}`}
+              className={`relative flex items-center justify-between gap-3 rounded-lg border border-x-0 border-t-0 border-gray-300 p-4 transition-all last:border-0 ${messageCheckStatus && messageCheckStatus[message.id] && "bg-gray-100 dark:bg-dark-150"}`}
             >
               <div className="flex items-start gap-5 md:items-center">
                 <div className="z-[2] flex flex-col-reverse md:flex-row md:items-center md:gap-5">
@@ -96,7 +118,11 @@ const InboxMessages: React.FC<InboxMessagesProps> = ({
                     suppressHydrationWarning
                     className={`${displayTools ? "block" : "hidden"} border-2 border-gray-400 data-[state=checked]:border-black data-[state=checked]:text-gray-200 dark:data-[state=checked]:border-white dark:data-[state=checked]:bg-white dark:data-[state=checked]:text-dark-50 md:block`}
                     onClick={() => {
-                      toggleMessageCheckStatus(message.id);
+                      toggleMessageStatus(
+                        message.id,
+                        setMessageCheckStatus,
+                        !messageCheckStatus[message.id],
+                      );
                     }}
                     checked={
                       messageCheckStatus !== undefined &&
@@ -106,20 +132,44 @@ const InboxMessages: React.FC<InboxMessagesProps> = ({
                   <Button
                     className="items-start bg-transparent p-0 pt-1 hover:bg-transparent md:items-center md:pt-0"
                     onClick={() => {
-                      setIsStarred(!isStarred);
+                      toggleMessageStatus(
+                        message.id,
+                        setStarredMessages,
+                        !starredMessages[message.id],
+                      );
+                      setActiveMessageData(message);
                     }}
                   >
-                    <MailStarredIcon starred={isStarred} />
+                    <MailStarredIcon starred={findStarredMessage(message.id)} />
                   </Button>
                 </div>
-                <div className="flex flex-col items-start justify-start gap-2 md:flex-row md:items-center">
+                <Link
+                  href={`/main/inbox/${inbox}/${message.id}`}
+                  className="z-[5] flex flex-col items-start justify-start gap-2 hover:text-links-background dark:hover:text-links-background md:flex-row md:items-center"
+                  onClick={() => {
+                    dispatch(inboxMessagesAction.setActiveMessage(message));
+                    localStorage.setItem(
+                      "activeMessage",
+                      JSON.stringify(message),
+                    );
+                  }}
+                >
                   <h3 className="w-40 font-bold">{message.sender.fullname}</h3>
                   <p className="text-gray-600 dark:text-gray-500">
                     {message.text.slice(0, isMobile ? 15 : 30)}...
                   </p>
-                </div>
+                </Link>
               </div>
-              <div className="text-xs">{retrieveDate(message.date)}</div>
+              <div className="text-xs">
+                {isMobile ? (
+                  <Avatar>
+                    <AvatarImage src={message.sender.profileImage} />
+                    <AvatarFallback></AvatarFallback>
+                  </Avatar>
+                ) : (
+                  retrieveDate(message.date, isMobile)
+                )}
+              </div>
               {isMobile && (
                 <div
                   className="absolute inset-0 z-[1]"
